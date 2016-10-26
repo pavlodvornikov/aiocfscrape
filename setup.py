b06c8b1554f67661598ce22385cd06250d3781c8
Pavlo Dvornikov <pavlodvornikov@gmail.com>
2016-10-26 14:02:50 +0300

    added gitignore, modified setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
   name = 'aiocfscrape',
   packages = ['aiocfscrape'],
   version = '0.0.1',
-  description = 'A simple async Python module to bypass Cloudflare\'s anti-bot page. See https://github.com/Anorov/cloudflare-scrape for more information.',
-  author = 'Anorov',
-  author_email = 'anorov.vorona@gmail.com',
-  url = 'https://github.com/Anorov/cloudflare-scrape',
+  description = 'A simple async Python module to bypass Cloudflare\'s anti-bot page. See https://github.com/pavlodvornikov/aiocfscrape for more information.',
+  author = 'pavlodvornikov',
+  author_email = 'pavlodvornikov@gmail.com',
+  url = 'https://github.com/pavlodvornikov/aiocfscrape',
   keywords = ['cloudflare', 'scraping'],
-  install_requires = ['Js2Py==0.37', 'requests >= 2.0.0']
+  install_requires = ['Js2Py==0.37', 'aiohttp >= 0.21.2']
 )
