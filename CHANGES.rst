481267302c1eb8c3ec9696e6d3474a1d59dbca00
Nachtalb <info@nachtalb.io>
2020-03-22 13:07:39 +0100

    Update information about the package

---
+++ CHANGES.rst
@@ -0,0 +1,61 @@
+Changelog
+=========
+
+
+0.0.9 (2019-03-21)
+------------------
+
+- Update anit-bot integration to latest cfscrape version [gaardiolor]
+
+
+0.0.8 (2019-03-16)
+------------------
+
+- Update anit-bot integration to latest cfscrape version [gaardiolor]
+
+
+0.0.7 (2019-01-10)
+------------------
+
+- Fix error when host does not return a "Server" header [pavlodvornikov]
+- Update anit-bot integration to latest cfscrape version [gaardiolor]
+
+
+0.0.6 (2018-06-19)
+------------------
+
+- Update readme [pavlodvornikov]
+
+
+0.0.5 (2018-06-19)
+------------------
+
+- Fix aiohttp version 3 compatibility [ape364]
+- Bump js2py [ape364]
+- Update anti-bot integration to latest cfscrape version [dteh]
+
+
+0.0.4 (2018-03-06)
+------------------
+
+- Update anti-bot integration to latest cfscrape version [slazarov]
+
+
+0.0.3 (2017-11-21)
+------------------
+
+- Fix AttributError [ape364]
+
+
+0.0.2 (2016-08-26)
+------------------
+
+- Support timeout inside request [pavlodvornikov]
+- Update package information [pavlodvornikov]
+- Remove obsolete files [pavlodvornikov]
+
+
+0.0.1 (2016-08-26)
+------------------
+
+- Initial implementation
--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup(
-    name = 'aiocfscrape',
-    packages = ['aiocfscrape'],
-    version = '0.0.10.dev0',
-    description = 'A simple async Python module to bypass Cloudflare\'s anti-bot page. See https://github.com/pavlodvornikov/aiocfscrape for more information.',
-    author = 'pavlodvornikov',
-    author_email = 'pavlodvornikov@gmail.com',
-    url = 'https://github.com/pavlodvornikov/aiocfscrape',
-    keywords = ['cloudflare', 'scraping', 'asyncio'],
-    install_requires = ['js2py==0.59', 'aiohttp >= 3.1.3, <4a']
-)
+version = '0.0.10.dev0'
+
+
+long_description = '{}\n\n{}'.format(open('README.rst').read(),
+                                     open('CHANGES.rst').read())
+
+setup(name='aiocfscrape',
+      version=version,
+      description='A simple async Python module to bypass Cloudflare\'s anti-bot page. '
+                  'See https://github.com/pavlodvornikov/aiocfscrape for more information.',
+      long_description=long_description,
+
+      author='pavlodvornikov',
+      maintainer='Nachtalb',
+      url='https://github.com/pavlodvornikov/aiocfscrape',
+      license='MIT',
+
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+          'js2py',
+          'aiohttp >= 3.1.3, <4a',
+      ],
+      )
